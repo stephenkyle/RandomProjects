8c89963da48523c6fdcc98aaeda5088777ddd297
stephenkyle <50040013+stephenkyle@users.noreply.github.com>
2021-01-21 14:24:49 -0500

    Update script.js

--- script.js
+++ script.js
@@ -42,15 +42,55 @@ function chanceCoin() {
 }
 
 function chanceCard() {
-
-	var cardSuit = ["Diamond", "Heart", "Spade", "Club"];
-	cs = Math.round(Math.random() * cardSuit.length);
+	// get how much is being betting
+	var bet = document.getElementById("cardInput").value;
+	// check if attempting to bet more than available
+	if (bet > currentMoney) {
+        alert("Cannot bet more money than you have.");
+    } else {
+    	// take bet from account
+    	currentMoney -= bet;
+    // get what user chose for suit
+	var suitBet = document.getElementById("cardSuitInput").value;
+	var cardSuit = ["Diamonds", "Hearts", "Spades", "Clubs"];
+	// get a random suit
+	cs = Math.round(Math.random() * (cardSuit.length - 1));
 	thisSuit = cardSuit[cs];
+	// display what suit was randomly chosen
+	document.getElementById("suit").innerHTML = thisSuit;
 
+	// get what use chose for value of card
+	var valueBet = document.getElementById("cardValueInput").value;
 	var cardValue = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
-	cv = Math.round(Math.random() * cardValue.length);
+	// get what use chose for value of card
+	cv = Math.round(Math.random() * (cardValue.length - 1));
 	thisValue = cardValue[cv];
+	// display what suit was randomaly chosen
+	document.getElementById("value").innerHTML = thisValue;
 
-	console.log(thisValue);
-	console.log(thisSuit);
+	// check if user input suit and value match
+	if (suitBet == thisSuit && valueBet == thisValue) {
+		bet *= 52;
+		console.log("right");
+		// if they won then add bet back to their current money
+		currentMoney += bet;
+	} else if (suitBet == thisSuit) {
+		bet *= 3;
+		currentMoney += bet;
+	} else if (valueBet == thisValue) {
+		bet *= 10;
+		currentMoney += bet;
+	}
+	document.getElementById("money").innerHTML = "$" + currentMoney;
+	}
 }
+
+
+// to do 
+// daily login
+// ai fighting?
+// slots
+// rolling dice
+// shooting game that rewards for money
+// loan system
+// lottery system
